6983d2142ecb7220cae42a691fcf579925c4e6ab
Zeynab sharifi <zeynab.sharifi.1378@gmail.com>
2021-09-14 02:45:44 +0430

    snake movement in snake game

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const snakGame =document.getElementById("snak");
 const ctx = snakGame.getContext('2d');
 
-ctx.fillStyle = '#121212'
-ctx.strokeStyle='#000'
-ctx.fillRect(0,0, snakGame.width, snakGame.height)
-ctx.strokeRect(0,0,snakGame.width,snakGame.height)
 
 
 let snake = [
@@ -15,15 +11,29 @@ let snake = [
     {x:110,y:150},
 ]
 
-snake.forEach(snakePart => {
-    ctx.fillStyle='red';
-    ctx.strokeStyle='#fff';
-
-    ctx.fillRect(snakePart.x , snakePart.y , 10 , 10);
-    ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10);
-})
 let foodX;
 let foodY;
+
+function main(){
+    setTimeout(() => {
+        clearCanvas();
+        drowFood();
+        advanceSnake();
+        drowSnake();
+
+        main()
+    }, 100);
+}
+
+let clearCanvas = ()=>{
+    ctx.fillStyle = '#121212'
+    ctx.strokeStyle='#000'
+
+    ctx.fillRect(0,0, snakGame.width, snakGame.height)
+    ctx.strokeRect(0,0,snakGame.width,snakGame.height)
+
+}
+
 
 let randomNumber=(max,min)=> Math.round((Math.random()*(max - min)+min)/10)*10
 let createfood = () =>{
@@ -35,12 +45,35 @@ let createfood = () =>{
          }
      })
 }
+
+let advanceSnake = () =>{
+    const head={x:snake[0].x+10,y:snake[0].y}
+
+    snake.unshift(head);
+    snake.pop();
+}
+
+let drowSnake = () =>snake.forEach(drowSnakePart)
+let drowSnakePart =snakePart => {
+    ctx.fillStyle='red';
+    ctx.strokeStyle='#fff';
+
+    ctx.fillRect(snakePart.x , snakePart.y , 10 , 10);
+    ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10);
+}
+
+let drowFood = () => {
+    ctx.fillStyle = 'green';
+    ctx.strokeStyle='green';
+
+    ctx.fillRect(foodX,foodY, 10, 10)
+    ctx.strokeRect(foodX,foodY, 10, 10)
+
+}
+
 createfood()
 
+main();
 
-ctx.fillStyle = 'green';
-ctx.strokeStyle='green';
 
-ctx.fillRect(foodX,foodY, 10, 10)
-ctx.strokeRect(foodX,foodY, 10, 10)
 
