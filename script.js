24942835cfea9940a82bfd04a8d39497e8283e11
Zeynab sharifi <zeynab.sharifi.1378@gmail.com>
2021-09-14 01:19:39 +0430

    add snake in canvas game snake

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ ctx.fillStyle = '#121212'
 ctx.strokeStyle='#000'
 ctx.fillRect(0,0, snakGame.width, snakGame.height)
 ctx.strokeRect(0,0,snakGame.width,snakGame.height)
+
+
+let snake = [
+    {x:150,y:150},
+    {x:140,y:150},
+    {x:130,y:150},
+    {x:120,y:150},
+    {x:110,y:150},
+]
+
+snake.forEach(snakePart => {
+    ctx.fillStyle='red';
+    ctx.strokeStyle='#fff';
+
+    ctx.fillRect(snakePart.x , snakePart.y , 10 , 10);
+    ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10);
+})
